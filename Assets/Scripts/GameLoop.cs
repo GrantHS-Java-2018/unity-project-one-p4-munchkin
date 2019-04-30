7e357bd35cf5cf678f23a1b134fa225001318863
Nathan Vogt <nathancvogt@gmail.com>
2019-04-30 15:29:14 -0700

    TERRAS LEAVING YEAA

---
+++ Assets/Scripts/GameLoop.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameLoop : MonoBehaviour
+{
+
+    public enum Stage
+    {
+        loadout,
+        kickdownDoor,
+        engageEntity,
+        flee,
+        fight,
+        deathOrLoot
+                
+    }
+
+    public Stage stage;
+
+    private void Awake()
+    {
+        stage = Stage.loadout;
+
+    }
+
+    private void Update()
+    {
+        
+    }
+}

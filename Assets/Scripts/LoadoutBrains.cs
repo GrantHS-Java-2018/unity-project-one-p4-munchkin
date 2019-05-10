f06cb7edaf3c96d858760c952061aa60a22a96bc
Nathan Vogt <nathancvogt@gmail.com>
2019-05-10 15:20:50 -0700

    Jakob is sus

---
+++ Assets/Scripts/LoadoutBrains.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadoutBrains : MonoBehaviour
+{
+
+   [SerializeField] private GameObject returnButton;
+
+
+   public void returnLoadout()
+   {
+      SceneManager.LoadScene("GamerTime");
+
+
+   }
+
+
+
+}

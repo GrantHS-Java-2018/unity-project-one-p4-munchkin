85f11c7d0b6146d85c64ba450a1a5450e85f0942
Terra <terrafleischer@gmail.com>
2019-06-04 14:59:59 -0700

    drag script added
    not working quite yet

---
+++ Assets/Scripts/Draggables.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Draggables : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
+{
+    
+    
+    public void OnBeginDrag(PointerEventData eventData){
+        Debug.Log("OnBeginDrag");
+        transform.GetSiblingIndex();
+        transform.SetAsLastSibling();
+    }
+
+    public void OnDrag(PointerEventData eventData){
+        Debug.Log("OnDrag");
+
+        this.transform.position = eventData.position;
+    }
+    
+    public void OnEndDrag(PointerEventData eventData){
+        Debug.Log("OnEndDrag");
+    }
+
+
+
+}
